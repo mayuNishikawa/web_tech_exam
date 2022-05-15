20289b1fd065c04a18b6e2a14e4ba7fa206a18c0
mayu nishikawa <sheseesseaontheseashore@gmail.com>
2022-05-15 17:57:29 +0900

    to get a hint

--- sample_pg.rb
+++ sample_pg.rb
@@ -7,9 +7,10 @@ begin
   result.each do |record|
     puts "ゴーヤの大きさ:#{record["weight"]} 売った相手:#{record["give_for"]}"
   end
-  result = connection.exec("select * from crops where not(give_for='自家消費'), quality=true;")
+  result = connection.exec("select * from crops where not(give_for='自家消費') AND quality=false;")
   result.each do |record|
     puts "譲渡先:#{record["give_for"]} 品質:#{record["quality"]}"
+  end
 ensure
   connection.finish
 end
